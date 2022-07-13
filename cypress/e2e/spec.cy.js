2dcef7808854f1effa99acbbf196df0924d34817
adrigog@gmail.com <adrigog@gmail.com>
2022-07-13 09:33:13 +0200

    Modifs fichier github_actions et fichier spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,12 +4,15 @@ describe('empty spec', () => {
     cy.get('input').eq(0).clear().type('200').type('enter')
     cy.get('input').eq(1).clear().type('3').type('enter')
     cy.get('input').eq(2).clear().type('1').type('enter')
-
-  }
+    cy.get('p').then((valeur) => {
+    var val = parseFloat(valeur[0].innerText.substr(27))     
+    expect(val).to.be.greaterThan(1000)
+      
+      
+    })
   
   
-  
-  )
+})
 
 
 
